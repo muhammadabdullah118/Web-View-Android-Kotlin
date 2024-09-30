4b8b8160dea6b0bfe735547b6b4fa2cf6b2fad4e
muhammadabdullah118 <muhammadabdullahattari.jk@gmail.com>
2024-09-30 20:24:31 +0500

    This commit include the  web view code.

---
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.webview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val wv_youtube = findViewById<WebView>(R.id.wv_url)
+
+        val wv_set = wv_youtube.settings
+
+        wv_set.javaScriptEnabled = true
+
+        wv_youtube.loadUrl("https://www.youtube.com/")
+
+        wv_youtube.webViewClient = object  : WebViewClient(){
+
+        }
+    }
+}
